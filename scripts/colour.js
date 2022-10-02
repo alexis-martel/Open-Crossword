a7921ea630b78b4e6689d923e2622ba94a8d11e6
alexis-martel <alex.is.here@icolud.com>
2022-10-02 16:37:21 -0400

    Remove green theme

--- scripts/colour.js
+++ scripts/colour.js
@@ -2,7 +2,6 @@ let colourSchemes = {
     "white": {"background": "#FFFAF0FF", "foreground": "#000000FF", "selection": "#B4D7FF", "highlight": "#DCDCDD"},
     "blue": {"background": "#0077ff", "foreground": "#000000FF", "selection": "#3724ff", "highlight": "rgb(1,102,225)"},
     "red": {"background": "#ff4f4f", "foreground": "#000000FF", "selection": "#ff6f6f", "highlight": "#c93f3f"},
-    "green": {"background": "#00ec2c", "foreground": "#000000FF", "selection": "#72e589", "highlight": "#00cc27"},
     "yellow": {"background": "#ffdb00", "foreground": "#000000FF", "selection": "#ffeb80", "highlight": "#e8c400"},
     "purple": {"background": "#7b49ff", "foreground": "#000000FF", "selection": "#926aff", "highlight": "#613ace"}
 };
