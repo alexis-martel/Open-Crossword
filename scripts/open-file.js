c6feb71e7b70acf1ccb942ccdf7cddc6807614f3
Alexis Martel <83471846+alexis-martel@users.noreply.github.com>
2022-12-03 13:57:58 -0500

    Add temporary fix for file opening without popup permissions #42

--- scripts/open-file.js
+++ scripts/open-file.js
@@ -9,14 +9,14 @@ function openFile() {
     let JSONFileInput = document.createElement("input");
     JSONFileInput.type = "file";
     JSONFileInput.accept = ".json";
-    JSONFileInput.multiple = true;
+    JSONFileInput.multiple = false;
     JSONFileInput.click();
     JSONFileInput.onchange = () => {
         let JSONFiles = JSONFileInput.files;
 
         for (const file of JSONFiles) {
-            file.text().then((text) => {
-                window.open(`solve.html?d=${encodeURIComponent(JSON.stringify(JSON.parse(text)))}`, '_blank');
+            file.text().then((fileContent) => {
+                window.location = `solve.html?d=${encodeURIComponent(JSON.stringify(JSON.parse(fileContent)))}`;
             });
         }
     }
