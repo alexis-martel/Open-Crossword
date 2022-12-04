762b00ced7f0a45a14ffb32411483054a49740a9
Alexis Martel <83471846+alexis-martel@users.noreply.github.com>
2022-12-03 21:26:26 -0500

    Fix relative URLs

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -560,7 +560,7 @@ let puzzle;
 
 if (params.has("p")) {
     let puzzleID = params.get("p").toString();
-    let puzzleURL = `../data/puzzles/${puzzleID}.json`;
+    let puzzleURL = `{{ site.baseurl }}/data/puzzles/${puzzleID}.json`;
     puzzle = new Puzzle(puzzleURL);
 } else if (params.has("d")) {
     let puzzleData = params.get("d").toString();
