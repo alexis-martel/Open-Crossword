c377535a9803be5813b4a9c5a3e8994fca5d1fef
alexis <alex.is.here@icloud.com>
2023-03-10 22:24:52 -0500

    Add offline fallback page #62

--- sw.js
+++ sw.js
@@ -1,6 +1,39 @@
 "use strict";
 
-self.addEventListener("install", (e) => {
-    console.info("Service worker install successful");
+const cacheVersion = 1; // Forces cache reload on increment
+const cacheName = "oc-cache";
+const offlinePageURL = "offline.html";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        await cache.add(new Request(offlinePageURL, {cache: "reload"})); // Forces network load if available
+    })());
+    self.skipWaiting().catch(console.error);
 });
 
+self.addEventListener("activate", (event) => {
+    event.waitUntil((async () => {
+        if ("navigationPreload" in self.registration) {
+            await self.registration.navigationPreload.enable();
+        }
+    })());
+    self.clients.claim().catch(console.error);
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith((async () => {
+            try {
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+                return await fetch(event.request);
+            } catch (error) {
+                const cache = await caches.open(cacheName);
+                return await cache.match(offlinePageURL);
+            }
+        })());
+    }
+});
